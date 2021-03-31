e07b09153503db4ec10120cc51d7edd94712ffc9
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-03-31 20:50:34 +0530

    Add static path and Static_root, staticfiles_dirs

--- rateyourreview/settings.py
+++ rateyourreview/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+location = lambda x: os.path.join(os.path.realpath(BASE_DIR), x)
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = location('public/static')
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
